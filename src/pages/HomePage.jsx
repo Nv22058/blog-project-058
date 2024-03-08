ea28f5dcf061e046a0e0e8c92a8dbdc79c3c12fb
Nv22058 <154870326+Nv22058@users.noreply.github.com>
2024-03-08 07:16:45 +0000

    viedo6

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import MainLayout from "../../src/components/MainLayout";
 import Hero from "./home/container/Hero";
+import Articles from "../components/ArticleCard";
 
 const HomePage = () => {
   return (
     <MainLayout>
       <Hero />
+      <Articles />
     </MainLayout>
   );
 };
