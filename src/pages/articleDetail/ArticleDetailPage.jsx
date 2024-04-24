486b201a00fb1f425292bde4c91cf7d428926168
Nv22058 <154870326+Nv22058@users.noreply.github.com>
2024-04-24 13:31:38 +0000

    video 11 done

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom";
 import BreadCrumbs from "../../components/BreadCrumbs";
 import CommentsContainer from "../../components/comments/CommentsContainer";
 import MainLayout from "../../components/MainLayout";
+import SocialShareButtons from "../../components/SocialShareButtons";
 import { images } from "../../constants";
 import SuggestedPosts from "./container/SuggestedPosts";
+
 const breadCrumbsData = [
   { name: "Home", link: "/" },
   { name: "Blog", link: "/blog" },
   { name: "Article title", link: "/blog/1" },
 ];
+
 const postsData = [
   {
     _id: "1",
@@ -36,6 +39,7 @@ const postsData = [
     createdAt: "2023-01-28T15:35:53.607+0000",
   },
 ];
+
 const tagsData = [
   "Medical",
   "Lifestyle",
@@ -45,6 +49,7 @@ const tagsData = [
   "Diet",
   "Education",
 ];
+
 const ArticleDetailPage = () => {
   return (
     <MainLayout>
@@ -78,14 +83,30 @@ const ArticleDetailPage = () => {
           </div>
           <CommentsContainer className="mt-10" logginedUserId="a" />
         </article>
-        <SuggestedPosts
-          header="Latest Article"
-          posts={postsData}
-          tags={tagsData}
-          className="mt-8 lg:mt-0 lg:max-w-xs"
-        />
+        <div>
+          <SuggestedPosts
+            header="Latest Article"
+            posts={postsData}
+            tags={tagsData}
+            className="mt-8 lg:mt-0 lg:max-w-xs"
+          />
+          <div className="mt-7">
+            <h2 className="font-roboto font-medium text-dark-hard mb-4 md:text-xl">
+              Share on:
+            </h2>
+            <SocialShareButtons
+              url={encodeURI(
+                "https://moonfo.com/post/client-side-and-server-side-explanation"
+              )}
+              title={encodeURIComponent(
+                "Client-side and Server-side explanation"
+              )}
+            />
+          </div>
+        </div>
       </section>
     </MainLayout>
   );
 };
+
 export default ArticleDetailPage;
