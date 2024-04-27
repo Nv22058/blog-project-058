dda6262a4246d1baa641bf2d75f95ef59e08aa15
Nv22058 <154870326+Nv22058@users.noreply.github.com>
2024-04-27 09:27:13 +0000

    20

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import store from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
