e5e8266baacd10a23a3332dd1e747a74e515deb0
Nv22058 <154870326+Nv22058@users.noreply.github.com>
2024-05-18 19:41:26 +0000

    27 - upload profile picture

--- src/services/index/users.js
+++ src/services/index/users.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+
 export const signup = async ({ name, email, password }) => {
   try {
     const { data } = await axios.post("/api/users/register", {
@@ -13,6 +14,7 @@ export const signup = async ({ name, email, password }) => {
     throw new Error(error.message);
   }
 };
+
 export const login = async ({ email, password }) => {
   try {
     const { data } = await axios.post("/api/users/login", {
@@ -26,6 +28,7 @@ export const login = async ({ email, password }) => {
     throw new Error(error.message);
   }
 };
+
 export const getUserProfile = async ({ token }) => {
   try {
     const config = {
@@ -33,6 +36,7 @@ export const getUserProfile = async ({ token }) => {
         Authorization: `Bearer ${token}`,
       },
     };
+
     const { data } = await axios.get("/api/users/profile", config);
     return data;
   } catch (error) {
@@ -53,6 +57,28 @@ export const updateProfile = async ({ token, userData }) => {
     const { data } = await axios.put(
       "/api/users/updateProfile",
       userData,
+      config
+    );
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const updateProfilePicture = async ({ token, formData }) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data } = await axios.put(
+      "/api/users/updateProfilePicture",
+      formData,
       config
     );
     return data;
